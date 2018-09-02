aa04cae9155d0c13870a89678adfdd132fd2ffc5
Momo Johnson <momo.johnson1987@gmail.com>
2018-09-02 13:01:33 -0500

    First commit of this serverless app

---
+++ index.js
@@ -0,0 +1,81 @@
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const AWS = require('aws-sdk');
+
+
+const USERS_TABLE = process.env.USERS_TABLE;
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+
+if(IS_OFFLINE === 'true'){
+    dynamoDb = new AWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+    });
+    console.log(dynamoDb);
+}else{
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+}
+
+app.use(bodyParser.json({ strict: false }));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+// Get User endpoint
+app.get('/users/:userId', function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  }
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not get user' });
+    }
+    if (result.Item) {
+      const {userId, name, email, firstName, lastName} = result.Item;
+      res.json({ userId, name, email, firstName, lastName});
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  });
+})
+
+// Create User endpoint
+app.post('/users', function (req, res) {
+  const { userId, name, email, firstName, lastName } = req.body;
+  if (typeof userId !== 'string') {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== 'string') {
+    res.status(400).json({ error: '"name" must be a string' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId: userId,
+      name: name,
+      email: email,
+      firstName: firstName,
+      lastName: lastName
+    },
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not create user' });
+    }
+    res.json({ userId, name, email, firstName, lastName });
+  });
+})
+
+module.exports.handler = serverless(app);
